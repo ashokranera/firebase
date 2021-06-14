35543dc7d58f149e8d1f582f663617624ae7f4c9
ashokranera <ashokranera>
2021-06-14 15:26:06 +0530

    first commit

---
+++ get.js
@@ -0,0 +1,52 @@
+"https://www.gstatic.com/firebasejs/8.6.3/firebase-app.js"
+"https://www.gstatic.com/firebasejs/8.6.3/firebase-auth.js"
+"https://www.gstatic.com/firebasejs/8.6.3/firebase-database.js"
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDFVgFLnz3CnBWrY0F3tANV3oX6wtzzHlg",
+    authDomain: "fir-javascripts.firebaseapp.com",
+    projectId: "fir-javascripts",
+    storageBucket: "fir-javascripts.appspot.com",
+    messagingSenderId: "304372702125",
+    appId: "1:304372702125:web:e6094b469db32dadd65f24"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function SelectAllData() {
+    firebase.database().ref('student').once('value', function(AllRecords) {
+        AllRecords.forEach(function(CurrentRecord) {
+            var name = CurrentRecord.val().NameOfStudent;
+            var roll = CurrentRecord.val().RollNo;
+            var sec = CurrentRecord.val().Sec;
+            var gen = CurrentRecord.val().Gender;
+            AddItemsToTable(name, roll, sec, gen);
+        });
+    })
+
+}
+
+window.onload = SelectAllData;
+
+var stdNo = 0;
+
+function AddItemsToTable(name, roll, sec, gen) {
+    var tbody = document.getElementById('tbody1');
+    var trow = document.createElement('tr');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    var td4 = document.createElement('td');
+    var td5 = document.createElement('td');
+    td1.innerHTML = ++stdNo;
+    td2.innerHTML = name;
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+    tbody.appendChild(trow);
+}
